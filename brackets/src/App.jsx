22fb75f6169a93f793b52fb7c5abcf7135aed2a7
aob123 <alexobeswick@gmail.com>
2024-01-20 03:46:08 +0100

    experimenting witth css grid for design

--- brackets/src/App.jsx
+++ brackets/src/App.jsx
@@ -1,35 +1,76 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <nav className="navBar">
+        <div className="navItems">
+          <h1>View</h1>
+          <h1>Brackets</h1>
+          <h1>Menu</h1>
+        </div>
+      </nav>
+      <main>
+        <div className="bracketView">
+          <div className="bracketGrid">
+            <div className="match match-1">
+              <div className="team-1">
+                <p>1. Sven</p>
+              </div>
+              <div className="team-2">
+                <p>2. Dave</p>
+              </div>
+            </div>
+            <div className="match match-2">
+              <div className="team-1">
+                <p>1. Pete</p>
+              </div>
+              <div className="team-2">
+                <p>2. John</p>
+              </div>
+            </div>
+            <div className="match match-3">
+              <div className="team-1">
+                <p>1. Mick</p>
+              </div>
+              <div className="team-2">
+                <p>2.Tina</p>
+              </div>
+            </div>
+            <div className="match match-4">
+              <div className="team-1">
+                <p>1. Lucy</p>
+              </div>
+              <div className="team-2">
+                <p>2. Roger</p>
+              </div>
+            </div>
+            <div className="match match-5">
+              <div className="team-1">
+                <p>1. Angela</p>
+              </div>
+              <div className="team-2">
+                <p>2. Michael</p>
+              </div>
+            </div>
+            <div className="match match-6">
+              <div className="team-1">
+                <p>1. Andy</p>
+              </div>
+              <div className="team-2">
+                <p>2. Jenny</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ brackets/src/components/brackets/Bracket.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Bracket = () => {
+  return <div>Bracket</div>;
+};
+
+export default Bracket;
